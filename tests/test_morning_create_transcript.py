48570f34bbd51e1f9820500089f38db0b3d1d7e5
Adonais Romero González <adonais.neoadonis@gmail.com>
2024-11-07 22:53:34 -0800

    Improvement to create_transcript test

--- tests/test_morning_create_transcript.py
+++ tests/test_morning_create_transcript.py
@@ -9,11 +9,16 @@ class TestMorningCreateMorningTranscriptMethods(unittest.TestCase):
 
     def test_get_metadata(self):
         """Test get_metadata function"""
-        m = create_transcript.get_metadata("morning/audio", "20231013")
-        self.assertEqual(m["episode"], "friday the 13th")
-        self.assertEqual(m["isSpecial"], True)
-        self.assertEqual(m["date"], "2023-10-13")
-        self.assertEqual(m["id"], "QW_VwFyUBeU")
+        metadata = create_transcript.get_metadata("morning/audio/20230731", "20230731")
+        self.assertEqual(metadata["episode"], "0")
+        self.assertEqual(metadata["isSpecial"], False)
+        self.assertEqual(metadata["date"], "2023-07-31")
+        self.assertEqual(metadata["id"], "Qd7ohtlOOkQ")
+        metadata = create_transcript.get_metadata("morning/audio/20240214", "20240214")
+        self.assertEqual(metadata["episode"], "BAU-lentine's day")
+        self.assertEqual(metadata["isSpecial"], True)
+        self.assertEqual(metadata["date"], "2024-02-14")
+        self.assertEqual(metadata["id"], "nMl8WQ1Iuls")
 
 
 if __name__ == "__main__":
