19f0d3aa29e508a1651ca0f8dd92e49beda7711e
Adonais Romero González <adonais.neoadonis@gmail.com>
2024-11-10 02:13:57 -0800

    Add transcript validation

--- morning/validate_docs.py
+++ morning/validate_docs.py
@@ -1,20 +1,24 @@
-"""Validate the structure and content of the transcripts and metadata files."""
+"""Validate the structure and content of the transcripts, summaries and metadata files."""
 
 import argparse
+import csv
 import os
 import json
 import subprocess
 import shutil
+from common import printer, transcript
 
 # Define constants
 SUMMARY_MAX_LENGTH = 4000
+HIGHLIGHTS_FILE = "config/highlights.csv"
+REPLACEMENTS_FILE = "config/replacements.csv"
 
 if __name__ == "__main__":
     os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
     # Parse command line arguments
     parser = argparse.ArgumentParser(
-        description="Validate the structure and content of the transcripts and metadata files."
+        description="Validate the structure and content of the transcripts, summaries and metadata files."
     )
     parser.add_argument(
         "--transcripts_dir",
@@ -28,24 +32,43 @@ if __name__ == "__main__":
 
     transcripts_dir_walker = os.walk(transcripts_dir)
 
+    with open(REPLACEMENTS_FILE, "r", encoding="utf-8") as csvfile:
+        replacements = list(csv.DictReader(csvfile))
+    with open(HIGHLIGHTS_FILE, "r", encoding="utf-8") as csvfile:
+        highlights = list(csv.DictReader(csvfile))
+
+    print("Validating transcripts...")
+
     for root, dirs, files in transcripts_dir_walker:
-        # Basic metadata checks
         for file in files:
+            # Basic metadata checks
             if file == "metadata.json":
                 with open(os.path.join(root, file), "r", encoding="utf-8") as f:
                     metadata = json.load(f)
                 relative_path = os.path.relpath(os.path.join(root, file), os.getcwd())
                 if metadata.get("episode") == "???":
-                    print(f"{relative_path} - Episode name/number not set")
+                    printer.print_error("Episode name/number not set", relative_path)
                 if isinstance(metadata["episode"], int) and metadata["isSpecial"]:
-                    print(f"{relative_path} - Numbered episode marked as special")
+                    printer.print_error(
+                        "Numbered episode marked as special", relative_path
+                    )
+            # Basic transcript checks
+            if file == "transcript.vtt":
+                transcript.check_captions(root, file)
+                transcript.fix_mistakes(replacements, root, file, True)
+                transcript.highlight_ambiguities(highlights, root, file)
 
-        # Check for missing summary files
+        # Check for missing files
         for directory in dirs:
             summary_path = os.path.join(root, directory, "summary.md")
+            transcript_path = os.path.join(root, directory, "transcript.vtt")
             relative_path = os.path.relpath(summary_path, os.getcwd())
             if not os.path.exists(summary_path):
-                print(f"{relative_path} - No summary file found")
+                printer.print_error("No summary file found", relative_path)
+            if os.path.exists(transcript_path):
+                file = transcript_path
+            else:
+                printer.print_error("No transcript file found", relative_path)
 
     # Check summaries formatting
     npm_command = shutil.which("bun")
